678ef6a6a43c7526dd611886a22474f5ba3c41a3
Abdelrahman Elsirafy <rahman2408@gmail.com>
2021-01-31 22:31:48 -0500

    commit tree cb9a245333d07d1e06f1aa973f39aabfe861b05e

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,22 +1,28 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
+
 require_relative './course.rb'
 
 class Scraper
+
   def get_page
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses")) 
-    doc.css(".post").each do |post|
+
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end 
+  
+ def get_courses
+  self.get_page.css(".post")
+ end
+
+ def make_courses
+      self.get_courses.each do |post|
       course = Course.new
       course.title = post.css("h2").text
-      course.schedule = post.css.(".date").text 
-      course.description = post.css("p").text 
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
     end
-    binding.pry
   end
-#Thank You Candice!
-Scraper.new.get_page
-
 
 def print_courses
     self.make_courses
@@ -28,9 +34,8 @@ def print_courses
       end
     end
   end
-  
 end
-
+Scraper.new.print_courses 
 
 
 # doc.css(".post").first.css("h2").text
